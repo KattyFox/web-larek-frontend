79d056c0effebea0ae0446340e9630a1770ad6d1
Ekaterina Verbilova <cverbilova@gmail.com>
2025-08-12 20:24:40 +0300

    checkpoint

---
+++ src/views/ItemShopView.ts
@@ -0,0 +1,14 @@
+import { ItemModel } from "../models/ItemModel";
+
+class ItemShopView {
+  model: ItemModel;
+
+  constructor(model: ItemModel) {
+    this.model = model;
+  }
+
+  
+}
+
+
+export {ItemShopView};
